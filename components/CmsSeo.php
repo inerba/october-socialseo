ec865c4fd17e1441d12805720f2876b91d50638d
Inerba <inerba@gmail.com>
2017-08-01 10:49:13 +0000

    no suffix/prefix in home page

--- components/CmsSeo.php
+++ components/CmsSeo.php
@@ -28,6 +28,7 @@ class CmsSeo extends ComponentBase
         $theme = Theme::getActiveTheme();
         $page = Page::load($theme,$this->page->baseFileName);
         $this->page["isCms"] = false;
+        $this->is_home = $this->page["is_home"] = false;
         
         if(is_null($page)) {
             $this->isCms = $this->page["isCms"] = true;          
@@ -39,6 +40,9 @@ class CmsSeo extends ComponentBase
         } elseif($page->hasComponent("customSeo")) {
             $this->isCms = $this->page["isCms"] = true;
         } else {
+            if($this->page->url == "/") {
+                $this->is_home = $this->page["is_home"] = true;
+            }
             $this->seo_title = $this->page["seo_title"]             = empty($this->page->meta_title) ? $this->page->title : $this->page->meta_title;
             $this->seo_description = $this->page["seo_description"] = Seo::render($this->page->meta_description);
             $this->seo_canonical = $this->page["seo_canonical"] = $this->page->seo_canonical;
